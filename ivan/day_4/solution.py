9925376c660b3c1da7c34e2db32f50f3715ca3a1
Ivan Toriya <toriya@precisdigital.com>
2023-12-04 18:12:47 +0100

    add Day 4: Scratchcards

--- ivan/day_4/solution.py
+++ ivan/day_4/solution.py
@@ -1,7 +1,6 @@
 # %%
 import os
 from collections import namedtuple
-from string import punctuation
 from typing import List
 
 from dotenv import load_dotenv
@@ -15,17 +14,43 @@ inp: str = utils.input.get(session_cookie=os.environ["SESSION_COOKIE"], year=202
 inp = inp.splitlines()
 
 # %% ==PART_1==
+
+Card = namedtuple("Card", "id, copies")
+
+cards = []
+
 total_points = 0
 for line in inp:
     card = line.split(": ")
-    id = card[0].split()[1]
+    id = int(card[0].split()[1])
     numbers = card[1].split(" | ")
     winning_numbers = set(numbers[0].split())
     my_numbers = set(numbers[1].split())
     matching = winning_numbers & my_numbers
     total_points += 2 ** (len(matching) - 1) if matching else 0
 
-print(total_points)
+    cards.append(Card(id, len(matching)))
+
+print("Part 1: ", total_points)
+
+# %% ==PART_2==
 
 
-# %%
+def calc_won_copies(deck: List[Card], cache={}):
+    if deck == []:
+        return 0
+
+    copies = []
+    for card in deck:
+        if not card.copies:
+            continue
+        if card not in cache:
+            won = deck[card.id : card.id + card.copies]
+            cache[card] = won
+        else:
+            won = cache[card]
+        copies.extend(won)
+    return len(copies) + calc_won_copies(copies)
+
+
+print("Part 2: ", calc_won_copies(cards) + len(cards))
